673887a93f8a932fa096cb30c9f1af59909e4731
Wulferis <acer1171090@gmail.com>
2024-11-08 18:18:22 +1100

    Fixed OutputAttatchmentDescription not implementing true Equals

--- src/Veldrid/OutputAttachmentDescription.cs
+++ src/Veldrid/OutputAttachmentDescription.cs
@@ -32,12 +32,29 @@ namespace Veldrid
         }
 
         /// <summary>
+        /// Element-wise equality.
+        /// </summary>
+        /// <param name="other">The instance to compare to.</param>
+        /// <returns>True if all elements are equal; false otherswise.</returns>
+        public override readonly bool Equals(object? obj) => obj is OutputAttachmentDescription other && Equals(other);
+
+        /// <summary>
         /// Returns the hash code for this instance.
         /// </summary>
         /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>
         public override int GetHashCode()
         {
             return (int)Format;
+        }
+
+        public static bool operator ==(OutputAttachmentDescription left, OutputAttachmentDescription right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(OutputAttachmentDescription left, OutputAttachmentDescription right)
+        {
+            return !(left == right);
         }
     }
 }
